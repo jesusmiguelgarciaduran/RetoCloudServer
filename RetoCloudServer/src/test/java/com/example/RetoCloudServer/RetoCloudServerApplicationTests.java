9ea45e61a935d3a50b58c0cdf0442c4ae4b15910
jesusmiguelgarciaduran <jesusmiguelgarciaduran@gmail.com>
2021-10-22 12:31:41 +0200

    Servidor completo

---
+++ RetoCloudServer/src/test/java/com/example/RetoCloudServer/RetoCloudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetoCloudServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCloudServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
